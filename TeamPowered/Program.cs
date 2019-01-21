6491119d7b11fc3e219c0cc094d14c9baa85269a
Wille <meander.william@gmail.com>
2019-01-21 14:32:29 +0100

    First commit!

---
+++ TeamPowered/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TeamPowered
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
