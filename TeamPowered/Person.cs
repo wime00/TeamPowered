431222f694cf56f63d3c9442bf5429caa7a535b7
Wille <meander.william@gmail.com>
2019-01-21 14:38:23 +0100

    Split Name into first and last name

--- TeamPowered/Person.cs
+++ TeamPowered/Person.cs
@@ -6,7 +6,8 @@ namespace TeamPowered
 {
     class Person
     {
-        public string Name { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
         public string Address { get; set; }
     }
 }
